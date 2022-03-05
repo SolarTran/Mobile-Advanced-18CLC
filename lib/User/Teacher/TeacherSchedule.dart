b5166582954126b83f8ecf54451954670018949c
Sang <enpro1955@gmail.com>
2022-03-05 22:55:12 +0700

    [UI] Teacher Schedule

--- lib/User/Teacher/TeacherSchedule.dart
+++ lib/User/Teacher/TeacherSchedule.dart
@@ -1,181 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/User/Teacher/progress_widget.dart';
-import 'light_color.dart';
-import 'text_styles.dart';
-import 'theme.dart';
-import 'rating_star_widget.dart';
-import 'extention.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
-class DetailScreen extends StatefulWidget {
-  const DetailScreen({Key? key,}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
 
   @override
-  _DetailPageState createState() => _DetailPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _DetailPageState extends State<DetailScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  Widget _appbar() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: <Widget>[
-        BackButton(color: Theme.of(context).primaryColor),
-        IconButton(
-          icon: const Icon(Icons.favorite, color: Colors.red),
-          onPressed: () {
-          },
-        )],
-    );
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    TextStyle titleStyle = TextStyles.title.copyWith(fontSize: 25).bold;
-    if (AppTheme.fullWidth(context) < 393) {
-      titleStyle = TextStyles.title.copyWith(fontSize: 23).bold;
-    }
-    return Scaffold(
-      backgroundColor: LightColor.extraLightBlue,
-      body: SafeArea(
-        bottom: false,
-        child: Stack(
-          children: <Widget>[
-            Image.asset('assets/images/photography.png'),
-            DraggableScrollableSheet(
-              maxChildSize: .8,
-              initialChildSize: .6,
-              minChildSize: .6,
-              builder: (context, scrollController) {
-                return Container(
-                  height: AppTheme.fullHeight(context) * .5,
-                  padding: const EdgeInsets.only(
-                    left: 19,
-                    right: 19,
-                    top: 16,
-                  ), //symmetric(horizontal: 19, vertical: 16),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(30),
-                      topRight: Radius.circular(30),
-                    ),
-                    color: Colors.white,
-                  ),
-                  child: SingleChildScrollView(
-                    physics: const BouncingScrollPhysics(),
-                    controller: scrollController,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        ListTile(
-                          title: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: <Widget>[
-                              Text('Teacher 1', style: titleStyle,),
-                              const SizedBox(width: 10),
-                              Icon(
-                                Icons.check_circle,
-                                size: 18,
-                                color: Theme.of(context).primaryColor,
-                              ),
-                              const Spacer(),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 10),
-                                child: RatingStarWidget(
-                                  rating: 5,
-                                ),
-                              ),
-                            ],
-                          ),
-                          subtitle: Text('English Teacher', style: TextStyles.bodySm.subTitleColor.bold),
-                        ),
-                        const Divider(thickness: .3, color: LightColor.grey),
-                        Row(
-                          children: <Widget>[
-                            ProgressWidget(
-                              value: 79.2,
-                              totalValue: 100,
-                              activeColor: LightColor.purpleExtraLight,
-                              backgroundColor: LightColor.grey.withOpacity(.3),
-                              title: "Good Review",
-                              durationTime: 500,
-                            ),
-                            ProgressWidget(
-                              value: 93.2,
-                              totalValue: 100,
-                              activeColor: LightColor.purpleLight,
-                              backgroundColor: LightColor.grey.withOpacity(.3),
-                              title: "Total Score",
-                              durationTime: 300,
-                            ),
-                            ProgressWidget(
-                              value: 85.2,
-                              totalValue: 100,
-                              activeColor: LightColor.purple,
-                              backgroundColor: LightColor.grey.withOpacity(.3),
-                              title: "Satisfaction",
-                              durationTime: 800,
-                            ),
-                          ],
-                        ),
-                        const Divider(thickness: .3, color: LightColor.grey),
-                        Text("About", style: titleStyle).vP16,
-                        Text(
-                          'Hello there! I am an Industrial Engineer in the profession but chose to do online teaching because I love to meet different learners. I am an outgoing person and I have this passion for dealing with different people and seeing them progress with',
-                          style: TextStyles.body.subTitleColor,
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: <Widget>[
-                            Container(
-                              height: 45,
-                              width: 45,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: LightColor.grey.withAlpha(150),
-                              ),
-                              child: const Icon(Icons.call, color: Colors.white),
-                            ).ripple(() {},
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            const SizedBox(width: 10),
-                            Container(
-                              height: 45,
-                              width: 45,
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                color: LightColor.grey.withAlpha(150),
-                              ),
-                              child: const Icon(Icons.chat_bubble, color: Colors.white),
-                            ).ripple(() {},
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            const SizedBox(width: 10),
-                            Container(
-                              height: 45,
-                              decoration: BoxDecoration(
-                                  color: Colors.lightBlue,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: TextButton(
-                                onPressed: () {},
-                                child: Text("Make an appointment", style: TextStyles.titleNormal.white),
-                              ),
-                            ),
-                          ],
-                        ).vP16
-                      ],
-                    ),
-                  ),
-                );
-              },
-            ),
-            _appbar(),
-          ],
-        ),
-      ),
+    return SfCalendar(
+      view: CalendarView.week,
+      firstDayOfWeek: 6,
+      dataSource: MeetingDataSource(getAppointments()),
     );
   }
 }
+
+List<Appointment> getAppointments() {
+  List<Appointment> meetings = <Appointment>[];
+  final DateTime today = DateTime.now();
+  final DateTime startTime =
+  DateTime(today.year, today.month, today.day, 9, 0, 0);
+  final DateTime endTime = startTime.add(const Duration(hours: 2));
+
+  meetings.add(Appointment(
+      startTime: startTime,
+      endTime: endTime,
+      subject: 'Board Meeting',
+      color: Colors.blue,
+      recurrenceRule: 'FREQ=DAILY;COUNT=10',
+      isAllDay: false));
+
+  return meetings;
+}
+
+class MeetingDataSource extends CalendarDataSource {
+  MeetingDataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -8,22 +8,25 @@ import 'Authentication/ForgotPassword/OtpVerify.dart';
 import 'Courses/CourseDetail.dart';
 import 'Courses/ListCourses.dart';
 import 'User/Teacher/ListTeachers.dart';
-import 'User/Teacher/TeacherSchedule.dart';
+import 'User/Teacher/TeacherDetail.dart';
 import 'User/Teacher/doctor_model.dart';
 import 'User/UserAppSettings.dart';
 import 'User/UserProfile.dart';
 
-void main() => runApp(MyApp());
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primaryColor: Color.fromRGBO(58, 66, 86, 1.0), fontFamily: 'Raleway'),
-      home: DetailScreen(),
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
